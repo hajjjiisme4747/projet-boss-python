d71fda2b3e10d79166114471333f86e243550485
hajjjiisme4747 <hajji.abdelilah@etu.uae.ac.ma>
2026-03-30 02:38:12 +0100

    Add print statement for debugging

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from gestion_stock.produit_utils import Produit
 
 produit1 = Produit("Gillette", "19DH")
 
-
+print("Message siftou Ayoub mn GitHub f smaa")
 produit1.stock()
 produit1.demande()
 produit1.stock()
